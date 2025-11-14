f572d35b47a0c39898e03ab68c5ac123025f64f4
najasinis <jinyeonge1234@naver.com>
2025-11-15 07:56:40 +0900

    create: Repository 인터페이스 생성

---
+++ backend/src/main/java/com/eoullim_backend/repository/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.eoullim_backend.repository;
+
+import com.eoullim_backend.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
+    List<Comment> findByUserId(Long userId);
+}
